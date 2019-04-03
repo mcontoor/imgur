7ea2d7df9ce5599a9e20dce3e674e2808c2cdcda
mcontoor <mcontoor@gmail.com>
2019-04-03 17:45:22 +0530

    upload img feature

--- imgur/backend/server.js
+++ imgur/backend/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const mongose = require('mongoose');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 var cors = require('cors');
 // const Images = require('./models/images');
@@ -10,6 +10,17 @@ app.use(cors());
 const router = express.Router();
 let Images
 
+
+// const uri = "mongodb+srv://mcontoor:mcontoor123@healme-bwqpu.mongodb.net/humgur?retryWrites=true";
+// mongoose.connect(uri, { useNewUrlParser: true })
+// .then(() =>
+//     console.log("Connected"),
+//         db = mongoose.db("humgur"),
+//         Images = mongoose.db("humgur").collection("images"),
+// )
+// .catch(err => console.log(err))
+
+ 
 const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://mcontoor:mcontoor123@healme-bwqpu.mongodb.net/humgur?retryWrites=true";
 const client = new MongoClient(uri, { useNewUrlParser: true });
@@ -18,14 +29,20 @@ client.connect(err => {
         console.log("$$$$$$$$$$$$$4", err);
     } else {
         console.log("Connected");
-        const db = client.db("humgur"); 
-        Images = client.db("humgur").collection("images");
-
         // collection.insertOne({"name": "Sai"})
         // perform actions on the collection object
         // client.close();
     }
 });
+// const db = client.db("humgur")
+// const collectionImages = client.db("humgur").collection("images");
+
+// app.use(express.static(path.join(__dirname, 'imgur/public')));
+
+// app.get('*', (req,res) =>{
+//     res.sendFile(path.join(__dirname+'/imgur/public/index.html'));
+// });
+
 router.get('/', (req, res) => {
     res.json({text: 'SOAL'})
 })
@@ -40,6 +57,32 @@ router.get('/getData', (req, res) => {
         }
     })
 })
+
+const storage = multer.diskStorage({
+    destination: client.db.collection('images'),
+    filename: function(req, file, cb){
+       cb(null,"IMAGE-" + Date.now() + path.extname(file.originalname));
+    }
+ });
+ 
+ const upload = multer({
+    storage: storage,
+    limits:{fileSize: 1000000},
+ }).single("myImage");
+ 
+
+ 
+ router.post("/upload", function(req, res) {
+    upload(req, res, (err) => {
+       console.log("Request ---", req.body),
+       console.log("Request file ---", req.file);//Here you get file.
+       /*Now do where ever you want to do*/
+       if (!err) {
+         return res.send(200).end();
+       }
+          
+    });
+ });
 
 app.use('/api', router);
 
--- imgur/client/src/App.js
+++ imgur/client/src/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './NavBar/NavBar'
+import ReactUploadImage from './upload.js'
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      data: '',
-    }
-  }
+  // constructor() {
+  //   super();
+  //   this.state = {
+  //     data: '',
+  //   }
+  // }
   handleClick = () => {
     fetch('http://localhost:5001/api/').then( data => data.json())
     .then( res => {
@@ -21,11 +23,27 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <button onClick={this.handleClick}>Click to get data</button>
-        <div>{this.state.data}</div>
+        {/* <button onClick={this.handleClick}>Click to get data</button>
+        <div>{this.state.data}</div> */}
+        <NavBar/>
+        <p>Work in Progress</p>
+        <ReactUploadImage />
       </div>
     );
   }
 }
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div>
+        // <NavBar/>
+        // <p>Work in Progress</p>
+        // <ReactUploadImage />
+//       </div>
+//     );
+//   }
+// }
+
 
 export default App;
